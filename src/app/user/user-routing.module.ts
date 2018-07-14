07f78da62b9ab0db3238f4c6ef48accbcb8827a0
Siddharth Dagar <siddharthn210@gmail.com>
2018-07-14 20:15:28 +0530

    Implemented Own profile edit functionality

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -6,6 +6,7 @@ import { routeConstants } from '../shared/app-properties';
 
 import { ChangePasswordComponent } from './change-password.component';
 import { UserComponent } from './user.component';
+import { DevoteeProfileComponent } from '../devotee/devotee-profile.component';
 
 const routes: Routes = [
     {
@@ -16,6 +17,11 @@ const routes: Routes = [
             {
                 path: routeConstants.changePassword,
                 component: ChangePasswordComponent,
+                canActivate: [BasicAuthGuardService],
+            },
+            {
+                path: routeConstants.editProfile,
+                component: DevoteeProfileComponent,
                 canActivate: [BasicAuthGuardService],
             }
         ]
